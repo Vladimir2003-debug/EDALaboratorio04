1f5e2497be454c0b549d726e2b1886053c3e02b6
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-06-12 06:24:09 -0500

    el nodo de la clase linkedList

---
+++ src/TDA/Node.java
@@ -0,0 +1,35 @@
+package TDA;
+
+public class Node<E> {
+    protected E data;
+    protected Node<E> next;
+
+    public Node(E data) {
+        this.data = data;
+    }
+
+    public Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+ 
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public String toString() {
+        return this.data.toString();
+    }
+}
