732b8234764a83d6f7121e87f3bb5252be7ee73f
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-06-12 15:51:09 -0500

    añadido cambios para que ordene por inserccion, a medias. El archivo genera una serie de simulaciones con peor caso

--- src/Ejercicios/Ejercicio1.java
+++ src/Ejercicios/Ejercicio1.java
@@ -1,30 +1,40 @@
 package Ejercicios;
 import TDA.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 public class Ejercicio1 {
     public static void main(String[] args) {
-        LinkedList<Integer> lista = new LinkedList<Integer>();
-        lista = generarPeorCasoInt(5);
-        System.out.println(lista);
-        System.out.println(insertionSort(lista));
-        System.out.println("Lista ordenada");
-        System.out.println(lista);
+        int tamano;
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Introduzca el tamaño máximo del último arreglo: ");
+        tamano = teclado.nextInt();
+
+        ArrayList<LinkedList<Integer>> casos = new ArrayList<LinkedList<Integer>>();
+        for (int n = 1; n <= tamano; n++) {
+            casos.add(generarPeorCasoInt(n));
+        }
+        Iterator<LinkedList<Integer>> puntero = casos.iterator();
+        while (puntero.hasNext()) {
+            System.out.println(insertionSort(puntero.next()));
+        }
     }
 
-
-    public static<E extends Comparable<E>> long insertionSort(LinkedList<E> list){
-        long start = System.nanoTime();
-        for(int i = 1; i < list.size(); i++){
-            E key = list.get(i);
-            int j = i - 1;
-            System.out.println(list);
-            while(j >= 0 && list.get(j).compareTo(key) > 0){
-                list.set(j + 1, list.get(j));
-                j--;
+    public static <E extends Comparable<E>> long insertionSort(LinkedList<E> lista) {
+        E key;
+        int i;
+        long nano_startTime = System.nanoTime();
+        for (int j = 1; j < lista.size(); j++) {
+            key = lista.get(j);
+            i = j - 1;
+            while (i > -1 && lista.get(i).compareTo(key) > 0) {
+                lista.set(i + 1, lista.get(i));
+                i--;
             }
-            list.set(j + 1, key);
+            lista.set(i + 1, key);
         }
-        long end = System.nanoTime();
-        return end - start;
+        long nano_endTime = System.nanoTime();
+        return nano_endTime - nano_startTime;
     }
     
     public static LinkedList<Integer> generarPeorCasoInt(int t){
