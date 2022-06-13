6f22fa2b98f858b0becf19678045487c78ea7df7
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-06-12 22:40:02 -0500

    El archivo puede agregar datos a un archivo

--- src/Ejercicios/Ejercicio1.java
+++ src/Ejercicios/Ejercicio1.java
@@ -3,21 +3,35 @@ import TDA.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Ejercicio1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int tamano;
         Scanner teclado = new Scanner(System.in);
         System.out.print("Introduzca el tamaño máximo del último arreglo: ");
         tamano = teclado.nextInt();
 
         ArrayList<LinkedList<Integer>> casos = new ArrayList<LinkedList<Integer>>();
+        String archivoInsercion = "insercionLinkedList.txt";
+        PrintWriter oS = new PrintWriter(archivoInsercion);
+
         for (int n = 1; n <= tamano; n++) {
             casos.add(generarPeorCasoInt(n));
         }
         Iterator<LinkedList<Integer>> puntero = casos.iterator();
         while (puntero.hasNext()) {
-            System.out.println(insertionSort(puntero.next()));
+            oS.println(String.valueOf(insertionSort(puntero.next())));
         }
+        oS.close();
+        // plot "/Users/richarteq/eclipse-workspace/Algoritmica/insercion.txt" with
+        // lines
+        // JavaPlot p = new JavaPlot();
+        // p.addPlot("\"/home/richart/eclipse-workspace/Algoritmica/insercion.txt\" with
+        // lines");
+        // p.plot();
+        
     }
 
     public static <E extends Comparable<E>> long insertionSort(LinkedList<E> lista) {
@@ -27,6 +41,8 @@ public class Ejercicio1 {
         for (int j = 1; j < lista.size(); j++) {
             key = lista.get(j);
             i = j - 1;
+            //System.out.println(lista);
+            
             while (i > -1 && lista.get(i).compareTo(key) > 0) {
                 lista.set(i + 1, lista.get(i));
                 i--;
